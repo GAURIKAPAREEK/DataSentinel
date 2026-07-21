acb05c25504abfba3df73556f4c41b3766f08507
GAURIKAPAREEK <156682622+GAURIKAPAREEK@users.noreply.github.com>
2026-07-21 23:35:35 +0530

    Update stylesheet.py

--- src/ui/stylesheet.py
+++ src/ui/stylesheet.py
@@ -1579,6 +1579,7 @@ div[class*="hdr_delete_account_btn"] button {{
     height: 28px !important;
     min-height: 28px !important;
     padding: 0 14px !important;
+    margin-left: 8px !important;
     font-size: 11px !important;
     background: var(--ui-gradient) !important;
     color: #ffffff !important;
